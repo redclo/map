cd51dba6c25f25063160beedb7310f651bfe8910
liwei <liwei@infish.cn>
2022-11-10 09:21:04 +0800

    update

--- scripts/deployHtmlToServer.js
+++ scripts/deployHtmlToServer.js
@@ -5,7 +5,7 @@ client.scp(
         host: '47.243.162.145',
         username: 'root',
         password: 'funeral@2022',
-        path: '/var/www/love/map/',
+        path: '/var/www/love/maptest/',
     },
     function(err, sftp) {
         console.log('uploadError:', err);
